951561c3843c6adc1a9247a06765524dc0d2b5ea
Bj190 <bryn.jones019@gmail.com>
2023-11-13 18:12:26 +0000

    .Map/Syntax error

--- src/main/kotlin/Record.kt
+++ src/main/kotlin/Record.kt
@@ -6,16 +6,16 @@ import kotlin.jvm.JvmRecord as JvmRecord1
 //need to inisliase record then it codes data properly with group by so it's sperated into String etc then it can be used for inputs and scanners
 // ins -> data/groupby -> instructions to use
 
-data class Student(val firstName: String, val  lastName: String, val Age: Int, val courseName: String, val courseModule: Int)
+data class Student(val firstName: String, val  lastName: String, val age: Int, val courseName: String, val courseModule: Int)
 fun record() {
 
     //@JvmRecord1
     val file = Paths.get("StudentDatabase.txt")
-    var studentlines = Files.readAllLines(file)
+    val studentlines = Files.readAllLines(file)
     "$studentlines"
         .trim(' ')
         .split(",")
-        .map {Student([0],[1],[2],[3])}
+        .map {Student(" "," ",0," ",0)}//map to list of Student as an array "need to know proper syntax?"
         .forEach {println(it)}
         //sperate by comma
         //trim

cac324340048ce263cd563309e1392c9550ea82d
Bj190 <bryn.jones019@gmail.com>
2023-12-28 21:47:33 +0000

    Init of Coursework

--- src/main/kotlin/View.java
+++ src/main/kotlin/View.java
@@ -8,13 +8,12 @@ public class View extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         var view = new TimetableView();
-        ScrollPane sp = new ScrollPane(); //not sure how this works yet further research
+        ScrollPane sp = new ScrollPane(); //not sure how this works yet further research is supposed to make the stage scrollable
 
         stage.setScene(new Scene(view));
         stage.setResizable(true);
-        stage.setHeight(1500); //this is not suitable for different screens //maybe get width and height of screen then divide?
-        stage.setWidth(1500);
-        stage.;
+        stage.setHeight(stage.getHeight()*10); //this is not suitable for different screens //maybe get width and height of screen then divide?
+        stage.setWidth(stage.getWidth()*20); // trying to get the screen resolution of the whichever device it loads and then adjust it
         stage.show();
 
     }
